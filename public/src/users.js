3dc7ef516133df76809ee14809c30cf394b7613a
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-07-16 16:02:50 -0400

    deleted som unused js files

--- public/src/users.js
+++
@@ -1,7 +0,0 @@
-var testDiv = document.getElementById('testDiv')
-var testForm = document.getElementById('testForm')
-
-var ddw = {name: 'darique'}
-testForm.addEventListener('submit', (e)=>{
-   testDiv.innerHTML =`${ddw}` 
-})
