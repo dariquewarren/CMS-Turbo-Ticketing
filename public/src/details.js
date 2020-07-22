321fe875d1302fb4a3b76d42978b183feb723a04
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-07-21 21:08:07 -0400

    setup user data to load into inputs

--- public/src/details.js
+++ public/src/details.js
@@ -7,52 +7,30 @@ var jobTitle = document.getElementById('jobTitle')
 var userAbout = document.getElementById('userAbout')
 var userAge = document.getElementById('userAge')
 
-var realAge = userAge.value
-var realName = userName.value
-var realJob = jobTitle.value
-var realAbout = userAbout.value
 
-var getRealage = ()=>{
-    if (!userAge.value){
-        realAge = '1999999'
-    } else{
-        realAge =userAge.value
-    }
-}
-var getRealName = ()=>{
-    if(!userName.value){
-        realName = 'noname'
-    } else{
-        realName = userName.value
-    }
-}
 
-var getRealJob =()=>{
-    if(!realJob.value){
-        realJob = 'none'
-            } else{
-                realJob = jobTitle.value
-            }
-}
 
-var getRealAbout=()=>{
-     if(!realAbout.value){
-        realAbout = 'private'
-            } else{
-                realAbout = userAbout.value
-            }
+var getUserData = ()=>{
+    fetch('/users/me').then((response)=>{
+        response.json().then((data)=>{
+            console.log('get',data)
+           userAge.value =`${data.age}`
+           userName.value =`${data.name}`
+           jobTitle.value =`${data.jobtitle}`
+           userAbout.value = `${data.about}`
+        })
+    })
 }
+getUserData()
+
 updateUserForm.addEventListener('submit', (e)=>{
     e.preventDefault()
-getRealage()
-getRealName()
-getRealJob()
-getRealAbout()
+
 var userUpdate = {
-    age: realAge,
-  name: realName,
-  jobtitle: realJob,
-  about: realAbout,
+    age: userAge.value,
+  name: userName.value,
+  jobtitle: jobTitle.value,
+  about: userAbout.value,
 
 }
 
