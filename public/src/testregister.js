25d91c88c091684ff8bb4e82f5a912cf8c36eb85
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-07-23 19:34:10 -0400

    UPDATED CREATE USER TO MATCH UPDATTE USER

--- public/src/testregister.js
+++ public/src/testregister.js
@@ -1,40 +1,27 @@
-/*
-{
-    "name":"darique warren",
-    "email": "ddwarren@swag.com",
-    "password": "harlemworld"
-} 
-
-*/
-
 
 var userForm = document.getElementById('createUserForm')
-var userage = document.getElementById('userAge')
 var usertitle = document.getElementById('userTitle')
 var userabout = document.getElementById('userAbout')
 var username = document.getElementById('userName')
 var useremail = document.getElementById('userEmail')
 var userpassword = document.getElementById('userPassword')
+var birthdayInput = document.getElementById('birthdayInput')
+var birthmonthInput = document.getElementById('birthmonthInput')
 
 userForm.addEventListener('submit', (e)=>{
     e.preventDefault()
 
-/*
-{
-  userage 
-  usertitle
-  userabout
-    "name":"darique warren",
-    "email": "ddwarren@swag.com",
-    "password": "harlemworld"
-} 
-
-*/
+   
+    var realDay = birthdayInput.value
+        var realMonth = birthmonthInput.value
+        var realBirthday = realDay +'-'+ realMonth
+        console.log(realBirthday)
+       console.log(typeof realBirthday)
 
 var ddw = {
   about: userabout.value,
   jobtitle: usertitle.value,
-  age: userage.value,
+  birthday: realBirthday,
     name: username.value,
     email: useremail.value,
     password: userpassword.value
@@ -47,9 +34,9 @@ console.log(ddw.name)
           },
         body: JSON.stringify(ddw)
       }).then(response => response.json()).then(data => {
-        console.log('Success:', data.user);
+        console.log('Success:', data);
         alert(`${data.user.name}, THANK YOU FOR SIGNING UP! PLEASE LOGIN`)
-        // location.assign('/login.html')
+        location.assign('/login.html')
       }).catch((error) => {
         console.error('Error:', error);
       })
