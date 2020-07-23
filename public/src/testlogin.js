8dccd0a0cbc93ebb30dc1d4ed9c1c61de647da89
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-07-23 13:59:52 -0400

    updated logout function and route. edited header

--- public/src/testlogin.js
+++ public/src/testlogin.js
@@ -1,6 +1,3 @@
-
-
-
 var loginForm = document.getElementById('loginForm')
 var emailInput = document.getElementById('emailInput')
 var passwordInput = document.getElementById('passwordInput')
@@ -29,16 +26,16 @@ console.log(ddw)
           'Content-Type': 'application/json',
           },
         body: JSON.stringify(ddw)
-      }).then(response => response.json()).then(data => {
+      }).then((response) => {response.json()}).then(data => {
         console.log('Success:', data);
-        // alert(`${data.email} Thank you for signing in`)
+       alert(`$ Thank you for signing in`)
+      location.assign('/index.html')
       }).catch((error) => {
         console.error('Error:', error);
+        alert('please try again',error )
       })
-      if(!document.cookie){
-return alert('incorrect credentials')
-      }
-      window.location.assign('/index.html')
+      
+    
 })
 
 var logoutForm = document.getElementById('logoutForm')
@@ -46,19 +43,11 @@ var logoutForm = document.getElementById('logoutForm')
 logoutForm.addEventListener('submit', (e)=>{
   e.preventDefault()
   
-console.log(document.cookie)
-document.cookie = "auth_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-console.log(document.cookie)
-alert(`YOU HAVE LOGGED OUT. COME BACK SOON`)
-window.location.assign('index.html')
+fetch('/users/logout').then((response)=>{
+  console.log(document.cookie)
+alert('you are now logged out')
+window.location.assign('/index.html')
+})
 
 })
 
-
-// var redirectForm = document.getElementById('redirectForm')
-
-// redirectForm.addEventListener('submit', (e)=>{
-//   e.preventDefault()
-//   var id = 12345
-// window.location.assign(`/index.html/${id}`)
-// })
