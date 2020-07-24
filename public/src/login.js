b1a4f9c5b4fedcabde288dcd0adc074b55393479
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-07-24 16:08:25 -0400

    added alert div to login pageand wired it up

--- public/src/login.js
+++ public/src/login.js
@@ -1,31 +1,66 @@
-var loginForm = document.getElementById('loginForm')
-var emailInput = document.getElementById('emailInput')
-var passwordInput = document.getElementById('passwordInput')
+console.log(document.cookies)
 
 
-loginForm.addEventListener('submit', (e)=>{
+let loginForm = document.getElementById('loginForm')
+let emailInput = document.getElementById('emailInput')
+let passwordInput = document.getElementById('passwordInput')
+let alertDiv = document.getElementById('alertDiv')
+let notLoggedInDiv = document.getElementById('notLoggedInDiv')
+loginForm.addEventListener('submit', async (e)=>{
     e.preventDefault()
-
+    var  successDiv = document.createElement('div')
+    successDiv.setAttribute('class', 'bg-dark text-primary ')
+    successDiv.setAttribute('role','alert')
 
 var ddw = {
     email: emailInput.value,
     password: passwordInput.value
 }
-console.log(ddw)
+
     fetch('/users/login',{
         method: 'POST', // or 'PUT' 
         headers: {
           'Content-Type': 'application/json',
           },
         body: JSON.stringify(ddw)
-      }).then((response) => {response.json()}).then(data => {
+      }).then(data => {
         console.log('Success:', data);
+            
+
+         /*
+ <div class="" role="alert">
+  This is a primary alert—check it out!
+</div>
+
+*/
+
+if(document.cookie.length === 0){
+ return location.assign('/login')
+} else{
+  notLoggedInDiv.innerHTML = `loading tickets.......`
+ 
+}
+
+setTimeout(()=>{
+  location.assign('/index')
+}, 3000)
+// if(!document.cookies){
+// notLoggedInDiv.innerHTML =`ooooops!`
+//   setTimeout
+// }else{
+//   location.assign('/index')
+// }
+
+
+
      
-     location.assign('/index')
+       
+      
       
       }).catch((error) => {
         console.error('Error:', error);
-        alert('please try again',error )
+        
+     
       })
       
     
