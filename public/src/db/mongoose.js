76a06afd0c992d0a80142a50fd30f04965c18977
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-07-27 17:09:42 -0400

    changed database connection to mongodb atlas database

--- public/src/db/mongoose.js
+++ public/src/db/mongoose.js
@@ -2,9 +2,10 @@ const validator = require('validator')
 const mongoose = require('mongoose')
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect('mongodb+srv://turboticket:turboticket@turboticket.3l28m.mongodb.net/turboticket?retryWrites=true&w=majority', {
     useNewUrlParser:true ,
     useCreateIndex: true, 
+    useUnifiedTopology: true,
     useFindAndModify: false
 })
 
