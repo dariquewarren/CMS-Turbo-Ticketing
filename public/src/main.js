4d9ea4b75ca107b676184773a36dc2a69a58ebce
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-07-28 12:25:14 -0400

    change env port to PORT

--- public/src/main.js
+++ public/src/main.js
@@ -8,7 +8,7 @@ const exphbs = require('express-handlebars')
 const express = require('express')
 require('./db/mongoose')
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 const cookieParser = require('cookie-parser')
 //DEFINE PATHS FOR EXPRESS CONFIG
