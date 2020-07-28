305ad029ef0aadb19aa6fe9d68b8274c07a0849c
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-07-28 12:29:43 -0400

    added env file. changed mongoose string uri to toi prcess env version

--- public/src/db/mongoose.js
+++ public/src/db/mongoose.js
@@ -1,12 +1,15 @@
 const validator = require('validator')
 const mongoose = require('mongoose')
 
+const dotenv = require('dotenv')
+dotenv.config()
 
-
-mongoose.connect('mongodb+srv://turboticket:turboticket@turboticket.3l28m.mongodb.net/turboticket?retryWrites=true&w=majority', {
+const uri = process.env.MONGODB_URI
+mongoose.connect(uri, {
     useNewUrlParser:true ,
     useCreateIndex: true, 
     useUnifiedTopology: true,
     useFindAndModify: false
 })
+console.log(uri)
 
