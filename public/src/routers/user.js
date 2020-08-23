8eaa290a413f34f1070fc6141fcec5d1e59cf492
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-08-23 15:04:09 -0400

    setup delete avatar route and route for viewing image

--- public/src/routers/user.js
+++ public/src/routers/user.js
@@ -46,6 +46,30 @@ router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res)=
 res.status(400).send({ error: error.message})
 })
 
+router.delete('/users/me/avatar', auth, async (req, res)=>{
+    req.user.avatar = undefined
+     await req.user.save()
+     
+     
+      res.send()
+  })
+ 
+router.get('/users/:id/avatar', async (req,res)=>{
+    try{
+        const user = await User.findById(req.params.id)
+        if(!user || !user.avatar){
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+
+    }catch(e){
+        res.status(404).send()
+    }
+})
+
+
 
 
 router.post('/users', async (req, res) => {
