0a3b3022c7e77f36e00f494df8cfcfaebafc2537
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2021-02-09 19:09:42 -0500

    added a guest login button to login page

--- public/src/login.js
+++ public/src/login.js
@@ -16,21 +16,22 @@ location.assign('/profile')
   })
 }
 
+let guestLoginButton = document.getElementById('guestLoginButton')
 let loginForm = document.getElementById('loginForm')
 let emailInput = document.getElementById('emailInput')
 let passwordInput = document.getElementById('passwordInput')
 let alertDiv = document.getElementById('alertDiv')
 let notLoggedInDiv = document.getElementById('notLoggedInDiv')
 
-loginForm.addEventListener('submit', async (e)=>{
-    e.preventDefault()
+guestLoginButton.addEventListener('click', (e)=>{
+e.preventDefault()
     var  successDiv = document.createElement('div')
     successDiv.setAttribute('class', 'bg-dark text-primary ')
     successDiv.setAttribute('role','alert')
 
-var ddw = {
-    email: emailInput.value,
-    password: passwordInput.value
+var testCredentials = {
+    email: 'test@test.com',
+    password: 'testtesttest'
 }
 
     fetch('/users/login',{
@@ -38,8 +39,9 @@ var ddw = {
         headers: {
           'Content-Type': 'application/json',
           },
-        body: JSON.stringify(ddw)
-      }).then(data => {
+        body: JSON.stringify(testCredentials)
+      }).then((data) => {
+        
         console.log('Success:', data);
             
 
@@ -66,14 +68,63 @@ notLoggedInDiv.innerHTML = `incorrect Email/Password. Redirecting...`
 setTimeout(()=>{
   location.assign('/index')
 }, 3000)
-
-
-
-
+    
+      }).catch((error) => {
+        console.error('Error:', error);
+        
      
-       
+      })
       
-      
+console.log('guest clicked')
+})
+
+
+loginForm.addEventListener('submit', async (e)=>{
+    e.preventDefault()
+    var  successDiv = document.createElement('div')
+    successDiv.setAttribute('class', 'bg-dark text-primary ')
+    successDiv.setAttribute('role','alert')
+
+var ddw = {
+    email: emailInput.value,
+    password: passwordInput.value
+}
+
+    fetch('/users/login',{
+        method: 'POST', // or 'PUT' 
+        headers: {
+          'Content-Type': 'application/json',
+          },
+        body: JSON.stringify(ddw)
+      }).then((data) => {
+        
+        console.log('Success:', data);
+            
+
+        /*
+ <div class="" role="alert">
+  This is a primary alert—check it out!
+</div>
+
+*/
+var timedreload = ()=>{
+  
+setTimeout(()=>{
+  location.assign('/login')
+},3000)
+}
+if(document.cookie.length === 0){
+notLoggedInDiv.innerHTML = `incorrect Email/Password. Redirecting...`
+ return timedreload()
+} else{
+  notLoggedInDiv.innerHTML = `loading tickets.......`
+ 
+}
+
+setTimeout(()=>{
+  location.assign('/index')
+}, 3000)
+    
       }).catch((error) => {
         console.error('Error:', error);
         
